a1274d69c69bc40be333a4296a00e49c04fabfc8
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-05-19 21:29:05 +0200

    Always load the action through Registry
    So the object will always be available anywhere, should it later be
    needed. And also, the new version of the router does not return a
    "callable" key.

--- Swf.php
+++ Swf.php
@@ -48,12 +48,8 @@ class Swf
             return;
         }
 
-        if ($route["callable"] === true) {
-            call_user_func_array($route["action"], $route["params"]);
-        } else {
-            $controller = Registry::setAlias("controller", "{$route["action"][0]}");
-            $controller->{$route["action"][1]}(...$route["params"]);
-        }
+        $controller = Registry::setAlias("controller", "{$route["action"][0]}");
+        $controller->{$route["action"][1]}(...$route["params"]);
 
         Hooks::call("bootstrap.after.route");
     }
